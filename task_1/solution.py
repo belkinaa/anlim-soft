41bf795635df6275283bf7a68cd5d25bbbeea397
Belkin Aleksej <belkin.a.a@inbox.ru>
2024-02-22 23:22:19 +0500

    + добавлено 2ое задание + рефакторинг решения 1го задания

--- task_1/solution.py
+++ task_1/solution.py
@@ -1,10 +1,9 @@
 import datetime
-def make_date(val):
-    format_date = "%Y-%m-%d"
-    return datetime.datetime.strptime(val, format_date)
 
-for rental_date_range_film in [(make_date("2020-1-1"), make_date("2020-1-7")),
-                               (make_date("2020-1-15"), make_date("2020-2-7"))]:
+make_str_date_to_datetime = lambda Ymd:  datetime.datetime.strptime(Ymd, "%Y-%m-%d")
+
+for rental_date_range_film in [map(make_str_date_to_datetime, ("2020-1-1", "2020-1-7")),
+                               map(make_str_date_to_datetime, ("2020-1-15", "2020-2-7"))]:
     start_rental, end_rental = rental_date_range_film
     date_rental = start_rental
     while date_rental <= end_rental:
