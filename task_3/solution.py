c736876e5594fdc4e06cd7294fc2dd38eec503c0
Belkin Aleksej <belkin.a.a@inbox.ru>
2024-02-24 18:34:10 +0500

    Завершение решения 3-го задания

--- task_3/solution.py
+++ task_3/solution.py
@@ -33,18 +33,45 @@ class Team():
 
 @dataclass()
 class Sport_game():
+    """
+    :param name : название соревнований
+    :param count_teams : количество команд, подавших заявку
+    :param count_teams_in_group : количество команд в группе
+    :param count_person_in_command : количество игроков в команде (в задании про это ничего нет, но решил просто сделать дополнительно (O_o))
+    """
     name: str
     count_teams: int
     count_teams_in_group: int
     count_person_in_command: int
-    list_teams: list = field(init=False, default_factory=list)
-    list_groups_teams: list = field(init=False, default_factory=list)
+
+    __registration_list_teams: list = field(init=False, default_factory=list)
+    __list_groups_teams: list = field(init=False, default_factory=list)
+
+    @property
+    def registration_list_teams(self):
+        return self.__registration_list_teams
+
+    @property
+    def list_groups_teams(self):
+        return self.__list_groups_teams
 
     def registration_commands(self):
         for number_commands in range(1, self.count_teams + 1):
-            self.list_teams.append(Team(f'Team#{number_commands}', self.count_person_in_command))
+            self.registration_list_teams.append(Team(f'Team#{number_commands}', self.count_person_in_command))
 
-    def round(self):
+    def set_rating(self, rating_win, rating_looser, is_win) -> int:
+        if is_win:
+            return rating_win + (rating_win + rating_looser) / 2
+        return rating_looser + (rating_win - rating_looser) / 2
+
+    def play_rounds(self):
+        """
+        - формирует группы случайным образом
+        - моделирует игру команд в группах (случайным образом выявляет победителя)
+        - в play_of выходят из групп только команды, занявшие 1, 2 и 3 места с очками 100, 75 и 50 соответственно
+        - по умолчанию в play_of сортировка команд по местам
+        :return:
+        """
         self.created_groups()
         self.game_groups()
 
@@ -54,15 +81,51 @@ class Sport_game():
                 if team.position < 4:
                     list_groups_teams.append(team)
 
-        list_groups_teams = sorted(list_groups_teams, key=lambda value: value.position)
+        while len(list_groups_teams) > 1:
+            list_groups_teams = self.round_play_of(list_groups_teams)
+            print('-'*10)
+
+    def round_play_of(self, list_groups_teams, is_rating=False, is_sorting=True) -> list:
+        """Формируется и возвращается список победителей игры моделью play_of
+        :param list_groups_teams: список команд для игры
+        :param is_sorting: делать ли сортировку списка команд
+        :param is_rating:  Сортировка групп для игры в play_of по умолчанию по рейтингу. Если указать False, то будет сортировка по месту на основе игр в группах
+        :return:
+        """
+        print(f'Start play_of. Count TEAM:{len(list_groups_teams)}')
+        play_of = []
+        if is_sorting or is_rating:
+            list_groups_teams = sorted(list_groups_teams, key=lambda value: value.rating if is_rating else value.position)
+
         for i in range(0, len(list_groups_teams), 2):
-            print(f'+--')
-            print(list_groups_teams[i])
+            team1 = list_groups_teams[i]
             if i+1 == len(list_groups_teams):
+                play_of.append(team1)
                 break
-            print(list_groups_teams[i+1])
+            team2 = list_groups_teams[i+1]
 
+            if random.randint(0, 1):
+                team1.rating = self.set_rating(team1.rating, team2.rating, True)
+                team1.list_win_team.append(team2.name)
+                team2.rating = self.set_rating(team1.rating, team2.rating, False)
+                team2.list_loose_team.append(team1.name)
+                play_of.append(team1)
+            else:
+                team2.rating = self.set_rating(team2.rating, team1.rating, True)
+                team2.list_win_team.append(team1.name)
+                team1.rating = self.set_rating(team2.rating, team1.rating, False)
+                team1.list_loose_team.append(team2.name)
+                play_of.append(team2)
+        print(f'Result play_of. Count TEAM:{len(play_of)}')
+        # Цикл для вывода на экран, чтобы посмотреть на итоги игры. На саму программу никак не влияет
+        for team_in_play_of in play_of:
+            print(team_in_play_of)
+            if len(play_of) == 1:
+                team1_team2 = [team1, team2]
+                team1_team2.remove(team_in_play_of)
+                print(team1_team2[0])
 
+        return play_of
 
     def set_position_team_group(self, group, position: list) -> None:
         """Устанавливает места командам в группе
@@ -75,7 +138,7 @@ class Sport_game():
                 2: 75,
                 3: 50
             }.get(team.position, 0)
-            print('team:', team)
+            print(team)
 
     def form_list_position_team(self, team, position: list) -> list:
         """Формируется список мест команд на основе "point" и их игр между собой"""
@@ -103,8 +166,8 @@ class Sport_game():
         """* Подсчет количества очков у команд в группах.
         * Распределение мест
         * устанавливается рейтинг"""
-        for group in self.list_groups_teams:
-            print('----')
+        for number_group, group in enumerate(self.list_groups_teams, start=1):
+            print(f'Result game group#{number_group}:')
             position = []
             for team in group.list_teams:
                 team.point = len(team.list_win_team)
@@ -128,32 +191,22 @@ class Sport_game():
 
     def created_groups(self):
         number_group = 0
-        while len(self.list_teams) > 0:
+        while len(self.registration_list_teams) > 0:
             number_group += 1
             group = Group(f'#{number_group}')
             for _ in range(self.count_teams_in_group):
-                group.list_teams.append((random_team := random.choice(self.list_teams)))
-                self.list_teams.remove(random_team)
-                if len(self.list_teams) == 0:
+                group.list_teams.append((random_team := random.choice(self.registration_list_teams)))
+                self.registration_list_teams.remove(random_team)
+                if len(self.registration_list_teams) == 0:
                     break
             self.list_groups_teams.append(group)
 
     def __post_init__(self):
         self.registration_commands()
 
-    def round2(self, list_groups_teams):
-        map_round = {}
-        for team in list_groups_teams:
-            if team.position not in map_round:
-                map_round[team.position] = [team]
-            else:
-                map_round[team.position].append(team)
-
-        for k, v in map_round.items():
-            print(k, v)
 
 
-
-
+print('!! В ЗАДАНИИ НАПСАНО СОРТИРОВКА В play_of ПО МЕСТАМ РЕЗУЛЬТАТА ИГР ГРУПП. ОДНАКО ЕСЛИ БУДЕТ НЕЧЕТНОЕ КОЛИЧЕСТВО КОМАНД, ТО ПОСЛЕДНЯЯ КОМАНДА В play_of БУДЕТ ИГРАТЬ ТОЛЬКО 1 ИГРУ - С ПОБЕДИТЕЛЕМ ВСЕХ ИГР play_of!')
+print('!! ТАКЖЕ В ЗАДАНИИ НЕТ ТЬОЧНОЙ ФОРМУЛЫ РАСЧЕТА РЕЙТИНГА, ПОЭТОМУ БЫЛА ПРИДУМАНА СВОЯ (О_o)')
 sport_game = Sport_game('Sample_game_name', 11, 4, 1)
-sport_game.round()
+sport_game.play_rounds()
